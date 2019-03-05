26db657f62a23f379c91471902d2b10c659501ac
Oliver <oliver@madcalm.com>
2019-03-05 11:07:21 +0000

    removed dummy unit test

--- UnitTests/Tests.cpp
+++ UnitTests/Tests.cpp
@@ -6,6 +6,6 @@ BOOST_AUTO_TEST_CASE(RectangleTestCase)
 {
 	CRectangle first(10, 10, 120, 120),second(40,40,10,10);
 	bool bIsEmpty = first.intersect(second).isEmpty();
-  BOOST_TEST(1 == 1);
+
   BOOST_TEST(bIsEmpty);
 }
