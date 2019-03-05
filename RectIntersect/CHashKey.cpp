d54586052256fafd8d2ab5cee2599f60ed030057
Oliver <oliveiros.cristina@gmail.com>
2019-03-05 15:13:38 +0000

    added new key object

--- RectIntersect/CHashKey.cpp
+++ RectIntersect/CHashKey.cpp
@@ -16,6 +16,35 @@ CHashKey::CHashKey(int n_first, int n_second)
 	SetBit(n_second);
 }
 
+CHashKey CHashKey::merge(const CHashKey& rc_second)
+{
+	CHashKey cReturnVal;
+	unsigned int nMax, nMin, nIndex,*pnBiggerArray;
+	
+
+	if (m_nLen > rc_second.m_nLen)
+	{
+		nMax = m_nLen;
+		nMin = rc_second.m_nLen;
+		pnBiggerArray = m_pnStorage;
+	}
+	else
+	{
+		nMin = m_nLen;
+		nMax = rc_second.m_nLen;
+		pnBiggerArray = rc_second.m_pnStorage;
+	}
+
+
+	cReturnVal.m_nLen = nMax;
+	cReturnVal.m_pnStorage = new unsigned int[nMax];
+	for (nIndex = 0; nIndex < nMin; nIndex++)
+		cReturnVal.m_pnStorage[nIndex] = m_pnStorage[nIndex] | rc_second.m_pnStorage[nIndex];
+	for (; nIndex < nMax; nIndex++)
+		cReturnVal.m_pnStorage[nIndex] = pnBiggerArray[nIndex];
+	return cReturnVal;
+}
+
 void CHashKey::SetBit(int n_triangle_num)
 {
 	unsigned int nOrMask;
--- RectIntersect/CHashKey.h
+++ RectIntersect/CHashKey.h
@@ -1,15 +1,44 @@
 #pragma once
 class CHashKey
 {
+	friend std::hash<CHashKey>;
 public:
 	CHashKey(int n_first,int n_second);
+
 	~CHashKey();
 
-
+	CHashKey merge(const CHashKey&);
 private:
+	CHashKey() {}
+
 	unsigned int *m_pnStorage;
 	unsigned int m_nLen;
 
 	void SetBit(int);
 };
 
+
+namespace std {
+
+	template <>
+	struct hash<CHashKey>
+	{
+		std::size_t operator()(const CHashKey& k) const
+		{
+			using std::size_t;
+			using std::hash;
+			using std::string;
+
+			// Compute individual hash values for first,
+			// second and third and combine them using XOR
+			// and bit shifting:
+			size_t returnVal = 0;
+
+			for (int i = 0; i != k.m_nLen; i++)
+				returnVal ^= hash<unsigned int>()(k.m_pnStorage[i]);
+			
+			return returnVal;
+		}
+	};
+
+}
