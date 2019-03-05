571061ce5fb978eb0e746c6e1cabccdaf0394c81
Oliver <oliveiros.cristina@gmail.com>
2019-03-05 12:20:05 +0000

    Added test cases

--- Rectangles/CRectangle.h
+++ Rectangles/CRectangle.h
@@ -16,12 +16,24 @@ public:
 		m_nY(n_y), 
 		m_nWidth(n_width), 
 		m_nHeight(n_height),
-	    m_bIsEmpty(false) {};
+	    m_bIsEmpty(false)  {};
 	~CRectangle();
 
 	CRectangle intersect(CRectangle& other);
 	string GetDescription();
-	bool isEmpty() { return m_bIsEmpty; }
+	bool isEmpty() const { return m_bIsEmpty; }
+
+	bool operator==(const CRectangle& other) const
+	{
+		if (m_bIsEmpty && other.isEmpty())
+			return true;
+		if (!m_bIsEmpty && !other.isEmpty())
+			return m_nX == other.m_nX && 
+			       m_nY == other.m_nY && 
+			       m_nWidth == other.m_nWidth && 
+			       m_nHeight == other.m_nHeight;
+		return false;
+	}
 
 private:
 	int m_nX;
